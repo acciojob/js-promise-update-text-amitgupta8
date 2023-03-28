a4b283842161e775c6c48723628745964b2b282e
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-29 03:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-//your JS code here. If required.
-function updateOutput() {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve('Hello, world!');
-          }, 1000);
-        });
-      }
-      
-      updateOutput().then((message) => {
-        document.getElementById('output').innerText = message;
-      });
+function updateText() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  })
+  .then(message => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = message;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+// Example usage:
+updateText();
